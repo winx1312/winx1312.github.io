ac1b25e6d54da14e12d289783bcdcd3343b92e8a
Ignat <decl011@tut.by>
2020-06-11 21:38:39 +0300

    no

--- test/src/components/error-indicator/error-indicator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from "./error-indicator.module.css"
-
-const ErrorIndicator = () => {
-  return <div className={s.error}>OOOPS, its Error</div>;
-};
-
-export default ErrorIndicator;
